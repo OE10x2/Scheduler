5daaf186eb66b67a54d3d70c272236c5631b2409
OE10x2 <OE10x2@gmail.com>
2019-12-14 16:02:39 -0500

    Done no work

---
+++ src/Algorithm2.java
@@ -0,0 +1,10 @@
+public class Algorithm2{
+    //Input: HashMap of ArrayList of Students
+    //Output: Sections
+    public static void main(String[] args){
+    }
+    //Method 1: finds all the 1-section courses
+    private void part1(){
+
+    }
+}
