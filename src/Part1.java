0e30397686ed9bafcb7bad57a9f86b1c04ebe499
OE10x2 <OE10x2@gmail.com>
2019-12-14 17:25:05 -0500

    Part1 is now in Part2

--- src/Part1.java
+++ src/Part1.java
@@ -1,44 +1,27 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Part1 {
-	
+public class Part1{
 	public HashMap<String, ArrayList<Student>> list = new HashMap<String, ArrayList<Student>>();
-	public void adjList() {
+	public void adjList(){
 		CourseInput c = new CourseInput();
 		StudentsInput s = new StudentsInput();
 		c.ReadInput("Courses.csv");
 		s.ReadStudentInput("Students.csv");
 		ArrayList<String> CourseList = c.CourseList;
-		for (int i = 0; i < c.getcourseSize(); i++) {
+		for (int i = 0; i < c.getcourseSize(); i++){
 			String curCourse = CourseList.get(i);
 			ArrayList<Student> students = new ArrayList<Student>();
-			for (int j = 0; j < s.StudentInfo.size(); j++) {
+			for (int j = 0; j < s.StudentInfo.size(); j++){
 				Student curStudent = s.StudentInfo.get(j);
-				for (int k = 0; k < 8; k++) {
-					
-					if (curStudent.getchoiceMain()[k].equals(curCourse)) {
+				for (int k = 0; k < 8; k++){
+					if (curStudent.getchoiceMain()[k].equals(curCourse)){
 						students.add(curStudent);
-						
 						break;
 					}
 				}
 			}
 			list.put(curCourse, students);
-			
 		}
 	}
-	
-	/*public static void main(String args[]) {
-		adjList();
-		ArrayList<Student> test = list.get("ICS2O1");
-		int cnt = 0;
-		for (int i=0;i<test.size();i++) {
-			Student xyzzy = test.get(i);
-			System.out.println(xyzzy.nameFirst);
-			cnt++;
-		}
-		System.out.println(cnt);
-	}*/
-	
 }
--- src/Part2.java
+++ src/Part2.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Part2{
     //Input: HashMap of ArrayList of Students
     //Output: Sections
     public static void main(String[] args){
-
+        Part1 lol = new Part1();
+        lol.adjList();
+        HashMap<String, ArrayList<Student>> list = lol.list;
+        ArrayList<Student> test = list.get("ICS2O1");
+        int cnt = 0;
+        for (Student i: test){
+            System.out.println(i.nameFirst);
+            cnt++;
+        }
+        System.out.println(cnt);
     }
     //Method 1: finds all the 1-section courses
-    private void part1(){
-
-    }
 }
