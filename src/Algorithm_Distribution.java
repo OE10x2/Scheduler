a2c391f3331ef6e347455e934151c0e754e8da8a
Arsen Cui <cuiarsen1@gmail.com>
2019-12-14 15:36:14 -0500

    Creates Algorithm_Distribution class with instructions

---
+++ src/Algorithm_Distribution.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Algorithm_Distribution {
+	
+	HashMap<Course, ArrayList<Student>> finalList = new ArrayList(); // list of courses with all students in it
+	
+	public void distributeStudents()
+	{
+		
+	}
+	
+	public 
+}
+/*
+ * find the amount of people who chose each course, based on that make a
+ * specific amount of sections for that course.
+ * 
+ * if people cant get their first choices, figure out if you can fit them into
+ * their alternative classes (make sure their alternative classes dont go above
+ * student limit)
+ * 
+ * make hashmap of arraylists. Hasmap is the name of course, arraylist stores the students in it
+ */
+//figure out how to give them alternatives if they cant get into their top choices
