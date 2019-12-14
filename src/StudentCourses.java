39bb17cd681176b4b3a8cac7dd8953cc134731e5
Danyang Wang <danyang0108@gmail.com>
2019-12-14 16:04:28 -0500

    Reads list of course names

--- src/StudentCourses.java
+++ src/StudentCourses.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class StudentCourses {
-
+	public static void main(String args[]) {
+		HashMap<String, ArrayList<Student>> list = new HashMap<String, ArrayList<Student>>();
+		
+	}
+	
 }
