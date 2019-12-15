3829afd775f288d33acfabcf79a15bfcb3f682cf
OE10x2 <OE10x2@gmail.com>
2019-12-14 21:43:19 -0500

    Hoang is Cool

--- src/Display.java
+++ src/Display.java
@@ -6,6 +6,10 @@ import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Display extends Application{
     public static void main(String[] args){
         launch(args);
@@ -13,16 +17,32 @@ public class Display extends Application{
 
     Group root = new Group();
     Scene scene = new Scene(root);
-
+    BorderPane pane = new BorderPane();
+    HBox profile = new HBox();
+    ArrayList<Final> students = new ArrayList<>();
 
     @Override
     public void start(Stage stage) throws Exception{
+        scene.getStylesheets().add("style1.css");
 
-        BorderPane pane = new BorderPane();
-        HBox profile = new HBox();
-        profile.getStylesheets().add("style1.css");
-        Text lol = new Text("Hoang is cool");
-        profile.getChildren().add(lol);
+        Scanner read = new Scanner(new File("results.csv"));
+        while (read.hasNextLine()){
+            String[] line = read.nextLine().split(" ");
+            String FN = line[0], LN = line[1];
+            int G = Integer.parseInt(line[2]);
+            String[] C = new String[8];
+            for (int i = 0; i < 8; i++) C[i] = line[i + 3];
+            Final current = new Final(line[0], line[1], Integer.parseInt(line[2]), C);
+            students.add(current);
+        }
+        for (Final student: students){
+            if (student.FN.equalsIgnoreCase("Divine")){
+                Text name = new Text(student.FN + " " + student.LN);
+                name.getStyleClass().add("text");
+                profile.getChildren().add(name);
+            }
+        }
+
         pane.setTop(profile);
         root.getChildren().add(pane);
 
